bf35878bbe2dfb3480215fb561d69e26238b3819
Irony <892768447@qq.com>
2020-09-08 16:52:34 +0800

    linux python

--- Linux/designer.sh
+++ Linux/designer.sh
@@ -6,6 +6,8 @@ basedir=`cd $basedir;pwd`
 cd -P ${basedir}
 
 export QT_DEBUG_PLUGINS=1
-export LD_LIBRARY_PATH=${basedir}/lib
+export PYTHONHOME=${basedir}
+export PYTHONPATH=${basedir}/local/lib/python3.6/:${basedir}/local/lib/python3.6/lib-dynload/:${basedir}/local/lib/python3.6/site-packages/:${basedir}/local/lib64/python3.6/site-packages/
+export LD_LIBRARY_PATH=${basedir}/local/lib64:${basedir}/lib
 
 ${basedir}/bin/designer $@
