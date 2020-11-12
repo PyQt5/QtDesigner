fd7c826665079c7143fc1868f0bb76e6357d6005
Irony <892768447@qq.com>
2020-11-12 13:07:31 +0800

    Qt Designer

---
+++ Linux/linguist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+basedir=`dirname $0`
+basedir=`cd $basedir;pwd`
+
+cd -P ${basedir}
+
+export QT_DEBUG_PLUGINS=1
+export PYTHONHOME=${basedir}
+export PYTHONPATH=${basedir}/local/lib/python3.6/:${basedir}/local/lib/python3.6/lib-dynload/:${basedir}/local/lib/python3.6/site-packages/:${basedir}/local/lib64/python3.6/site-packages/
+export LD_LIBRARY_PATH=${basedir}/local/lib64:${basedir}/lib
+
+${basedir}/bin/linguist $@
