4cb807f4f23fdc7e84c79e7cedee50c7f02dd729
Irony <892768447@qq.com>
2020-11-12 18:12:09 +0800

    assistant and linguist

--- Linux/assistant.sh
+++ Linux/assistant.sh
@@ -5,9 +5,8 @@ basedir=`cd $basedir;pwd`
 
 cd -P ${basedir}
 
-export QT_DEBUG_PLUGINS=1
-export PYTHONHOME=${basedir}
-export PYTHONPATH=${basedir}/local/lib/python3.6/:${basedir}/local/lib/python3.6/lib-dynload/:${basedir}/local/lib/python3.6/site-packages/:${basedir}/local/lib64/python3.6/site-packages/
-export LD_LIBRARY_PATH=${basedir}/local/lib64:${basedir}/lib
+#export QT_DEBUG_PLUGINS=1
+export LD_LIBRARY_PATH=${basedir}/lib:$LD_LIBRARY_PATH
 
-${basedir}/bin/assistant $@
+chmod +x ${basedir}/bin/assistant
+${basedir}/bin/assistant $@ &
--- Linux/designer.sh
+++ Linux/designer.sh
@@ -5,9 +5,10 @@ basedir=`cd $basedir;pwd`
 
 cd -P ${basedir}
 
-export QT_DEBUG_PLUGINS=1
+#export QT_DEBUG_PLUGINS=1
 export PYTHONHOME=${basedir}
 export PYTHONPATH=${basedir}/local/lib/python3.6/:${basedir}/local/lib/python3.6/lib-dynload/:${basedir}/local/lib/python3.6/site-packages/:${basedir}/local/lib64/python3.6/site-packages/
 export LD_LIBRARY_PATH=${basedir}/local/lib64:${basedir}/lib
 
-${basedir}/bin/designer $@
+chmod +x ${basedir}/bin/designer
+${basedir}/bin/designer $@ &
